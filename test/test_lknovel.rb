5923ef724e4ceaee27512c96e6ab06dd2cd90362
Zhong Jianxin <azuwis@gmail.com>
2014-03-10 16:36:26 +0800

    Remove test from skel

--- test/test_lknovel.rb
+++ test/test_lknovel.rb
@@ -1,11 +1,1 @@
 require 'minitest_helper'
-
-class TestLknovel < MiniTest::Unit::TestCase
-  def test_that_it_has_a_version_number
-    refute_nil ::Lknovel::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
-  end
-end
