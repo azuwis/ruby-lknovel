b74d3b5382be4f12cd268441892152eb6249da86
Zhong Jianxin <azuwis@gmail.com>
2014-03-10 16:34:22 +0800

    Add lknovel/utils
    Only has retryable for now

---
+++ lib/lknovel/utils.rb
@@ -0,0 +1,24 @@
+# http://blog.codefront.net/2008/01/14/retrying-code-blocks-in-ruby-on-exceptions-whatever/
+# Options:
+# * :tries - Number of retries to perform. Defaults to 3.
+# * :on - The Exception on which a retry will be performed. Defaults to Exception, which retries on any Exception.
+#
+# Example
+# =======
+#   retryable(:tries => 1, :on => OpenURI::HTTPError) do
+#     # your code here
+#   end
+
+def retryable(options = {}, &block)
+  opts = { :tries => 3, :on => Exception }.merge(options)
+
+  retry_exception, retries = opts[:on], opts[:tries]
+
+  begin
+    return yield
+  rescue retry_exception
+    retry if (retries -= 1) > 0
+  end
+
+  yield
+end
