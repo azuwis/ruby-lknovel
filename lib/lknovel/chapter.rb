786d89dda974ab9cb4bd860455433e4b272b74a1
Zhong Jianxin <azuwis@gmail.com>
2014-03-10 17:49:19 +0800

    Add lknovel/chapter and chapter template

---
+++ lib/lknovel/chapter.rb
@@ -0,0 +1,56 @@
+# encoding: utf-8
+require 'erb'
+require 'fileutils'
+require 'lknovel/image'
+require 'lknovel/meta'
+require 'lknovel/utils'
+require 'nokogiri'
+require 'open-uri'
+
+module Lknovel
+  class Chapter
+
+    attr_reader :url, :title, :content
+
+    def initialize(url)
+      @url = url
+      parse
+    end
+
+    def parse
+      page = retryable do
+        Nokogiri::HTML(open(@url))
+      end
+
+      @title = page.css('li.active')[0].text.sub('章', '章 ').strip
+
+      @content = []
+      page.css('div#J_view').css('div.lk-view-line, br + br').each do |x|
+        img = x.css('img[data-cover]')
+        if x.name == 'br'
+          @content.push('')
+        elsif img.length > 0
+          lk_image = Image.new(URI.join(url, img[0]['data-cover']))
+          @content.push(lk_image)
+        else
+          @content.push(x.text.strip.sub(/^　+/, ''))
+        end
+      end
+    end
+
+    def html(erb, path = nil)
+      template = erb.is_a?(ERB) ? erb : ERB.new(File.read(erb), nil, '-')
+      html_content = template.result(binding)
+      if path.nil?
+        html_content
+      else
+        if !File.exists?(path)
+          File.open(path, 'w') do |file|
+            file.puts html_content
+          end
+        end
+      end
+    end
+
+  end
+end
--- lib/lknovel/meta.rb
+++ lib/lknovel/meta.rb
@@ -1,3 +1,7 @@
 module Lknovel
   VERSION = "0.0.1"
+  MODULE_PATH = File.expand_path('..', __FILE__)
+  TEMPLATE_PATH = File.join(MODULE_PATH, 'templates')
+  IMAGE_DIR = 'images'
+  STYLESHEET_DIR = 'stylesheets'
 end
