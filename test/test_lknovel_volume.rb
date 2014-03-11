4ad62f6725b22a0907e2f843d42ef28dd108cdfe
Zhong Jianxin <azuwis@gmail.com>
2014-03-11 14:07:43 +0800

    Add test for lknovel/volume

---
+++ test/test_lknovel_volume.rb
@@ -0,0 +1,42 @@
+# encoding: utf-8
+require_relative 'minitest_helper'
+require 'lknovel/volume'
+
+describe Lknovel::Volume do
+
+  volume = Lknovel::Volume.new('http://lknovel.lightnovel.cn/main/book/2136.html')
+
+  it 'get path' do
+    volume.path.must_equal '打工吧！魔王大人 - 短篇01 - 短篇01'
+  end
+
+  it 'get author' do
+    volume.author.must_equal '和原聪司'
+  end
+
+  it 'get illustrator' do
+    volume.illustrator.must_equal '029'
+  end
+
+  it 'get publisher' do
+    volume.publisher.must_equal '电击文库'
+  end
+
+  it 'get date' do
+    volume.date.must_equal '2012-08-17 21:02:34'
+  end
+
+  it 'get intro' do
+    volume.intro.must_match /『拼死工作吧！魔王陛下！』/
+  end
+
+  it 'get cover image' do
+    volume.cover_image.file.must_equal '20120817210516_51962.jpg'
+  end
+
+  it 'generate front html' do
+    html = volume.html(File.join(Lknovel::TEMPLATE_PATH, 'front.html.erb'))
+    html.must_match '<li><a href="000.html">第0章 序章</a></li>'
+  end
+
+end
