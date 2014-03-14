8af32a1131865fd6a1eda44521f24b34dbe3c1a0
Zhong Jianxin <azuwis@gmail.com>
2014-03-14 16:27:37 +0800

    Update image.rb test for crop

--- test/test_lknovel_image.rb
+++ test/test_lknovel_image.rb
@@ -3,9 +3,26 @@ require 'lknovel/image'
 
 describe Lknovel::Image do
 
-  it 'lknovel image' do
-    image = Lknovel::Image.new('http://lknovel.lightnovel.cn/illustration/image/20120808/20120808202600_62961.jpg')
+  TMP = 'test/tmp'
+
+  FileUtils.mkdir_p TMP
+
+  image = Lknovel::Image.new('http://lknovel.lightnovel.cn/illustration/image/20120808/20120808202600_62961.jpg')
+
+  it '@file' do
     image.file.must_equal '20120808202600_62961.jpg'
+  end
+
+  it 'download' do
+    image.download(TMP)
+    File.exists?(File.join(TMP, image.file)).must_equal true
+  end
+
+  it 'crop' do
+    cover_image = File.join(TMP, 'cover.jpg')
+    cropped = image.crop(cover_image, '52%x100%+0+0', :dir => TMP) { |w, h| w > h * 1.5 }
+    cropped.must_equal true
+    File.exists?(cover_image).must_equal true
   end
 
 end
