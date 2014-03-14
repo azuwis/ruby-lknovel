cae5bb100649cda277796f251c79f9978ae7c94c
Zhong Jianxin <azuwis@gmail.com>
2014-03-14 16:26:50 +0800

    image.rb: Add crop function

--- lib/lknovel/image.rb
+++ lib/lknovel/image.rb
@@ -25,5 +25,27 @@ module Lknovel
       end
     end
 
+    def crop(output, operation, options = { :dir => '.' })
+      input = File.join(options[:dir], @file)
+      begin
+        dim = IO.popen(['identify', '-format', '%[fx:w] %[fx:h]', input])
+        dim = dim.gets.split
+        width = dim[0].to_i
+        height = dim[1].to_i
+      rescue Exception
+        width = 0
+        height = 0
+      end
+      need_crop = true
+      need_crop = yield(width, height) if block_given?
+      cropped = false
+      if need_crop
+        if system('convert', input, '-crop', operation, output)
+          cropped = true
+        end
+      end
+      cropped
+    end
+
   end
 end
