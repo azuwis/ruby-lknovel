6571251c88cb089bbc57c0a36e01d51fe9d46a41
Zhong Jianxin <azuwis@gmail.com>
2015-06-23 17:49:41 +0800

    Fix lknovel series test case

--- test/test_lknovel_series.rb
+++ test/test_lknovel_series.rb
@@ -7,8 +7,8 @@ describe Lknovel::Series do
   series = Lknovel::Series.new('http://lknovel.lightnovel.cn/main/vollist/615.html')
   series.parse
 
-  it 'volume[0]' do
-    volume = series.volumes[0]
+  it 'volume[1]' do
+    volume = series.volumes[1]
     volume.url.must_equal 'http://lknovel.lightnovel.cn/main/book/2123.html'
   end
 
